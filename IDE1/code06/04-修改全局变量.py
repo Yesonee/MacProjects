0f9a8b9e94cf0229ab49cf2c81db20a5106fb779
Yesonee <827567696@qq.com>
2021-12-11 11:50:55 +0800

    new

--- IDE1/code06/04-修改全局变量.py
+++ IDE1/code06/04-修改全局变量.py
@@ -1,17 +1,1 @@
-a = 100
-#print(a)
-
-def testA():
-    print(a)
-
-def testB():
-    #如果直接修改 此时的a是局部变量a
-    a = 300
-    print(a)
-
-def testC():
-    print(a)
-
-testA()
-testB()
-testC()
+a = 100#print(a)def testA():    print(a)def testB():    #如果直接修改 此时的a是局部变量a    a = 300    print(a)def testC():    print(a)testA()testB()testC()
