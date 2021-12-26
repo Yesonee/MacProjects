483d089199569fb99ca25249154c782598f84294
Yesonee <827567696@qq.com>
2021-12-26 18:00:33 +0800

    new

--- IDE1/code08/09、数据类型转换 重点.py
+++ IDE1/code08/09、数据类型转换 重点.py
@@ -1,3 +1,11 @@
 list1 = [10,20,30,20,40,50]
 s1 = {100,300,200,500}
 t1 = {'a','b','c','d','e'}
+
+#tuple():转换成元祖
+# print (tuple(list1))
+# print(tuple(s1))
+# list():转换成列表
+print(list(s1))
+print(list(t1))
+# set():转换成集合
